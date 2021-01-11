bf1b4adf3edfbf2feb0db01ebb782699865ec5a8
Nathani <a.nathani@dxc.com>
2021-01-11 12:24:23 +0530

    bodyupdate

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import React from 'react';
 import './App.css';
 // import Home from './Routers/home'
+// import Index from './Home/index'
 import Index from './Home/index'
+
 function App() {
   return (
- 
-    <div className="App">
-
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <h1>Welcome to Library</h1>
-      <h2>Welcome to Ratneswar Team</h2>
-      <button>Click</button>
-
-    </div>
+      <div>
+        <Index />
+      </div>
   );
 }
 
--- src/Home/index.js
+++ src/Home/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import Header from './components/header';
+import Footer from './footer';
+ import Header from './header';
+import SuggestionBox from './suggestionBox';
+// import Deleteit from './deleteIt';
+import data from './body.json'
+
+function getData() {
+    return data.Content;
+  }
 function index(){
+    const kids=getData()[0].kidsbooks;
+  
     return(
-        <Header />
+        <div>
+        {/* <Header />
+       <SuggestionBox title="kids" data= {kids}/><br></br><br></br> */}
+       <Header />
+       <SuggestionBox title="kids" data= {kids}/><br></br><br></br>
+       
+      
+       <Footer />
+        
+     
+       </div>
     )
 }
 
