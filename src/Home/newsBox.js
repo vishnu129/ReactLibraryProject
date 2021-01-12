fc0c577718a59136866da944e9523d8749e01f1a
S <vishnu.s2@dxc.com>
2021-01-12 17:13:49 +0530

    converted newsbox into data driven functional component

--- src/Home/newsBox.js
+++ src/Home/newsBox.js
@@ -1,94 +1,45 @@
-import React, { Component } from "react";
-import data from "./home.json";
+import React from "react";
 import { Carousel } from 'antd';
 import 'antd/dist/antd.css';
 
 import "./newsBox.css";
 
 
-class NewsBox extends Component {
-    //use the lifecycle method to get latest news from the database
-
-    /* componentDidMount()
-     {
-         this.getNewsHeadings();
-     }
- */
-  //in this function api call can be done and data fetched from api,
-    getData() {
-        return data.newsBox.newsHeadings;
-    }
-
-    render() {
-        const list = this.getData();
+function NewsBox(props) {
+        const list = props.news.newsHeadings;
 
         return (
             <div>
-                 <h1 className="title">{data.newsBox.title}</h1>
+                 <h1 className="title">{props.news.title}</h1>
             <Carousel autoplay>
-                <div>
+               { list.map(obj => {
+                    return(
+                        <div>
                     <div className="wrapper">
                         <div className="newsImage">
                             <img
                                 className="imageArea"
-                                src={list[0]["image-url"]} alt="image" />
+                                src={obj.imageUrl} alt="loading" />
                         </div>
                         <div className="newsContent">
-                            <h2>{list[0].heading}</h2>
-                            <h3>{list[0].summary}</h3>
+                            <h2>{obj.heading}</h2>
+                            <h3>{obj.summary}</h3>
                             <h3 className="seemore">see more...</h3>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div className="wrapper">
-                        <div className="newsImage">
-                            <img
-                                className="imageArea"
-                                src={list[1]["image-url"]} alt="image" />
-                        </div>
-                        <div className="newsContent">
-                            <h2>{list[1].heading}</h2>
-                            <h3>{list[1].summary}</h3>
-                            <h3 className="seemore">see more..</h3>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className="wrapper">
-                        <div className="newsImage">
-                            <img
-                                className="imageArea"
-                                src={list[2]["image-url"]} alt="image"/>
-                        </div>
-                        <div className="newsContent">
-                            <h2>{list[2].heading}</h2>
-                            <h3>{list[2].summary}</h3>
-                            <h3 className="seemore">see more..</h3>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className="wrapper">
-                        <div className="newsImage">
-                            <img
-                                className="imageArea"
-                                src={list[3]["image-url"]} alt="image"/>
-                        </div>
-                        <div className="newsContent">
-                            <h2>{list[3].heading}</h2>
-                            <h3>{list[3].summary}</h3>
-                            <h3 className="seemore">see more..</h3>
-                        </div>
-                    </div>
-                </div>
+              
+                    )
+                })
+            }
+                
             </Carousel>
             </div>
 
         )
     }
 
-}
+
 
 export default NewsBox;
 
