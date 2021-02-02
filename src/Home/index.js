be6e39be725d3b823eb235311c397ed3be199f8b
Nathani <a.nathani@dxc.com>
2021-02-02 09:15:18 +0530

    edit

--- src/Home/index.js
+++ src/Home/index.js
@@ -25,42 +25,9 @@ function index() {
      {/* <History /> */}
       {/* <Profile /> */}
        <HomeRouter />
-{/*import React, { Fragment } from "react";
-import Footer from "./footer";
-import Header from "./Header";
-import News from "./newsBox";
-import Quote from "./quotation";
-import SuggestionBox from "./suggestionBox";
-import data from "./body.json";
-import newsData from "./home.json";
-import homeRouter from "../Router/homeRouter";
-import blogdata from "./blogs.json";
-import Blog from './blogs';
-import Careers from './Careers';
 
-
-
-function index() {
-  const content = data.Content;
-  const news = newsData.newsBox;
-  let obj;
-  return (
-    <div>
-      <Header />
       
-      <Quote />
-      <News news = {news}/>
-      
-      {
-        content
-          .map(childObj => {
-            obj = childObj[Object.keys(childObj)]
-            return <SuggestionBox genre={obj}  />
-          }
-          )
-      } 
-      
-      <homeRouter/>  */}
+     
 {/* <Blog data={blogdata}/> */}
     </div>
   );
